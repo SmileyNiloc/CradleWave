c18997448c45b2523c0f052237af05054d6d3447
Colin Aten <caten@udel.edu>
2026-04-08 10:01:36 -0400

    Added local run MQTT test script

---
+++ mqtt_test/cradlewave_mqtt_test.py
@@ -0,0 +1,89 @@
+import json
+import time
+import random
+from awscrt import mqtt
+from awsiot import mqtt_connection_builder
+
+# Configure AWS IoT connection
+AWS_ENDPOINT = "a1py3mdrrjrz1-ats.iot.us-east-1.amazonaws.com"
+CLIENT_ID = "caten_laptop"
+TOPIC = "raw_sensor_data"
+
+ROOT_CA_PATH = "root-CA.crt"
+PRIVATE_KEY_PATH = "caten_laptop.private.key"
+CERTIFICATE_PATH = "caten_laptop.cert.pem"
+
+
+# Create a payload with a timestamp and an array of 2048 random float32 values
+def generate_payload():
+    ms_timestamp = time.time_ns() // 1_000_000
+    float_array = [round(random.uniform(0.0, 100.0), 5) for _ in range(2048)]
+    return {"timestamp": ms_timestamp, "data": float_array}
+
+
+# Create a payload with a timestamp data in json dumps format
+def generate_payload_json(data):
+    ms_timestamp = time.time_ns() // 1_000_000
+    package = {"timestamp": ms_timestamp, "data": data}
+    return json.dumps(package)
+
+
+if __name__ == "__main__":
+    start_timestamp = time.time()
+
+    # 1. Build the MQTT Connection using the AWS mTLS builder
+    print("Configuring AWS IoT connection...")
+    mqtt_connection = mqtt_connection_builder.mtls_from_path(
+        endpoint=AWS_ENDPOINT,
+        cert_filepath=CERTIFICATE_PATH,
+        pri_key_filepath=PRIVATE_KEY_PATH,
+        ca_filepath=ROOT_CA_PATH,
+        client_id=CLIENT_ID,
+        clean_session=False,
+        keep_alive_secs=30,
+    )
+
+    # 2. Connect
+    print(f"Connecting to {AWS_ENDPOINT}...")
+    # The SDK uses "futures" (promises) for asynchronous operations
+    connect_future = mqtt_connection.connect()
+
+    # .result() pauses the script until the connection is fully established
+    connect_future.result()
+    print("✅ Connected!")
+
+    # Fake Payload Generation
+    data = [round(random.uniform(0.0, 100.0), 5) for _ in range(2048)]
+
+    samples = 1000
+    # 3. Publish Data to MQTT Topic
+    for i in range(samples):
+        json_payload = generate_payload_json(data)
+        publish_future, packet_id = mqtt_connection.publish(
+            topic=TOPIC,
+            payload=json_payload,
+            qos=mqtt.QoS.AT_LEAST_ONCE,  # Equivalent to QoS 1
+        )
+        # print(
+        #     f"Publishing message {i+1}: {json_payload[:100]}... to topic '{TOPIC}'..."
+        # )
+        publish_future.result()
+    # Wait for the AWS server to acknowledge the message was received
+
+    print("✅ Message Published Successfully!")
+
+    # 4. Disconnect cleanly
+    print("Disconnecting...")
+    disconnect_future = mqtt_connection.disconnect()
+    disconnect_future.result()
+    print("Disconnected.")
+    total_time = time.time() - start_timestamp
+    print(f"Total time taken: {total_time:.2f} seconds")
+    print(
+        f"Sent {samples} frames in {total_time:.2f} seconds ({samples/total_time:.2f} frames/sec)"
+    )
+    print(f"Average time per frame: {total_time/samples:.4f} seconds")
+    print(f"Average payload size: {len(json_payload)} bytes")
+    print(
+        f"Average bytes per second: {(len(json_payload)*samples)/total_time:.2f} bytes/sec"
+    )
