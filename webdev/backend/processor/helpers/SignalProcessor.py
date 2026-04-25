c95cf3922de73f04de5faab6b7d4ac19b928b2a6
LBlackburn03 <loganblackburn03@gmail.com>
2026-04-25 01:08:41 -0400

    Updated SignalProcessor.py to current version.

--- webdev/backend/processor/helpers/SignalProcessor.py
+++ webdev/backend/processor/helpers/SignalProcessor.py
@@ -7,51 +7,37 @@ class SignalProcessor:
     # Implements signal processing pipeline
 
     def __init__(self, sample_rate):
-        self.sample_rate = sample_rate
-        self.nyquist = sample_rate / 2
+        self.sample_rate = sample_rate  # 15 frames per second default
+        self.nyquist = sample_rate / 2  # Nyquist frequency
+
+        # Heart Filters
+        # MTI Highpass Filter
         self.hp_b_heart, self.hp_a_heart = butter(3, 0.3 / self.nyquist, "highpass")
-        self.bp_b_heart, self.bp_a_heart = butter(
-            6, [0.8 / self.nyquist, 2.5 / self.nyquist], "band"
-        )
+        # Bandpass Filter: 48-150bpm range
+        self.bp_b_heart, self.bp_a_heart = butter(6, [0.8 / self.nyquist, 2.5 / self.nyquist], "band")
+
+        # Breathing filters
+        # MTI Highpass filter
         self.hp_b_breath, self.hp_a_breath = butter(3, 0.07 / self.nyquist, "highpass")
-        self.bp_b_breath, self.bp_a_breath = butter(
-            3, [0.07 / self.nyquist, 0.4 / self.nyquist], "band"
-        )
-        self.kernel_heart = np.ones(5) / 5
-        self.kernel_breath = np.ones(3) / 3
-        # self.window_size = max(3, int(0.5 * self.sample_rate))
+        # Bandpass Filter: 4-24bpm range
+        self.bp_b_breath, self.bp_a_breath = butter(3, [0.07 / self.nyquist, 0.4 / self.nyquist], "band")
+
+        # Smoothing Window Kernels
+        self.kernel_heart = np.ones(3) / 3      # Window Size = 3
+        self.kernel_breath = np.ones(10) / 10   # Window Size = 10
 
     # 1
     def moving_target_indicator_heart(self, signal_data):
         # MTI filter to remove static clutter
-        # Remove the DC component
-        signal_data = signal_data - np.mean(signal_data)
-
         # 3rd order butterworth high-pass filter
-        # sample rate = 15 Hz, nyquist = 7.5 Hz
-        # 0.3 / 7.5 = 0.4
-        return lfilter(self.hp_b_heart, self.hp_a_heart, signal_data)
+        return lfilter(self.hp_b_heart, self.hp_a_heart, signal_data - np.mean(signal_data))
 
-    def moving_target_indicator_breath(self, data):
-        data = data - np.mean(data)
-        return lfilter(self.hp_b_breath, self.hp_a_breath, data)
+    def moving_target_indicator_breath(self, signal_data):
+        # MTI filter to remove static clutter
+        # 3rd order butterworth high-pass filter
+        return lfilter(self.hp_b_breath, self.hp_a_breath, signal_data - np.mean(signal_data))
 
     # 2
-    def sliding_average_filter_heart(self, signal_data, window_size=5):
-        # Sliding average filter to remove impulse noise
-        if len(signal_data) < window_size:
-            return signal_data
-
-        # kernel = np.ones(window_size) / window_size
-        return np.convolve(signal_data, self.kernel_heart, mode="same")
-
-    def sliding_average_filter_breath(self, data, window=3):
-        if len(data) < window:
-            return data
-        # kernel = np.ones(window) / window
-        return np.convolve(data, self.kernel_breath, mode="same")
-
-    # 3
     def bandpass_filter_heart(self, signal_data):
         # Bandpass filter for heartbeat frequency range (48-150 bpm)
         if len(signal_data) < 20:
@@ -59,11 +45,27 @@ class SignalProcessor:
 
         return lfilter(self.bp_b_heart, self.bp_a_heart, signal_data)
 
-    def bandpass_filter_breath(self, data):
-        if len(data) < 20:
-            return data
-        return lfilter(self.bp_b_breath, self.bp_a_breath, data)
+    def bandpass_filter_breath(self, signal_data):
+        if len(signal_data) < 20:
+            return signal_data
+        return lfilter(self.bp_b_breath, self.bp_a_breath, signal_data)
 
+    # 3
+    def sliding_average_filter_heart(self, signal_data, window_size=3):
+        # Sliding average filter to remove impulse noise
+        if len(signal_data) < window_size:
+            return signal_data
+
+        # kernel = np.ones(window_size) / window_size
+        return np.convolve(signal_data, self.kernel_heart, mode="same")
+
+    def sliding_average_filter_breath(self, signal_data, window_size=10):
+        # Sliding average filter to remove impulse noise
+        if len(signal_data) < window_size:
+            return signal_data
+        # kernel = np.ones(window_size) / window_size
+        return np.convolve(signal_data, self.kernel_breath, mode="same")
+    
     # 4
     def estimate_heart_rate_fft(self, signal_data):
 
@@ -105,65 +107,98 @@ class SignalProcessor:
             return 0
 
         return bpm
-
+    
+    def heart_peak_detect(self, signal_data, frame_rate, window):
         """
-        # Remove DC and normalize
-        # signal_data = signal_data - np.mean(signal_data)
-        std = np.std(signal_data)
-        if std > 0:
-            signal_data /= std
+        Divide heart_filtered into 20 second windows and count peaks in each. 
         
-        nperseg = max(128, len(signal_data)//4)
-        nfft = max(512, len(signal_data)*2)
-        window = windows.hann(nperseg)
-        noverlap = int(0.5 * nperseg)
-
-        # Apply Welch PSD estimation
-        freqs, pxx = welch(
-            signal_data,
-            fs=self.sample_rate,
-            window=window,
-            nperseg=nperseg,
-            noverlap=noverlap,
-            nfft=nfft
-        )
+        heart_filtered:  1D filtered breathing signal
+        fs_breath:       sample rate of breath signal (default 15 same as FRAME_RATE)
+        window_sec:      window size in seconds (default 20)
         
-        # Focus on heart rate band (0.8–2.5 Hz)
-        mask = (freqs >= 0.8) & (freqs <= 2.5)
-        freqs_band = freqs[mask]
-        pxx_band = pxx[mask]
-        
-        if len(freqs_band) == 0:
-            return 0, 0
-        
-        # Find dominant frequency
-        peak_idx = np.argmax(pxx_band)
-        dominant_freq = freqs_band[peak_idx]
-        bpm = dominant_freq * 60
-        
-        # Confidence metric (power ratio)
-        mean_power = np.mean(pxx_band)
-        peak_power = pxx_band[peak_idx]
-        confidence = (peak_power / (mean_power + 1e-10))
-        
-        # Apply simple threshold to reject weak signals
-        if confidence < 2.0:
-            bpm = 0
-        
-        return bpm
+        returns: list of (time_sec, bpm) tuples
         """
+        # Extract Window
+        samples_per_window = int(window * frame_rate) # Default 300 samples at 15 Hz
+        # Total number of windows in recorded sample 
+        # >>>Sometimes total samples is slightly less than TIME * FRAME_RATE, check exact dimmensions for creating windows<<<
+        num_windows = (len(signal_data)+1) // samples_per_window
 
-    def estimate_breathing_rate_fft(self, data):
-        if len(data) < 60:
-            return 0
+        results = []
+        prev_val = 0
+        for i in range(num_windows):
+            chunk = signal_data[i * samples_per_window : (i + 1) * samples_per_window]
 
-        peaks, _ = find_peaks(data, height=1.5)
-        if len(peaks) == 0:
-            return 0
+            # Minimum distance between peaks = 0.4s = 1 sample at 2.5 Hz
+            # This corresponds to max 150 bpm
+            peaks, _ = find_peaks(chunk, distance=int(0.2 * frame_rate), prominence = 0.15)
 
-        time_window = len(data) / self.sample_rate
-        bpm = (len(peaks) / time_window) * 60
-        return bpm
+            num_beats = int(len(peaks))
+            
+            # Inter-window smoothing (avg prev value with current for new window)
+            # First run (no smoothing)
+            if prev_val == 0:
+                bpm = (num_beats / window) * 60
+                prev_val = bpm
+            # Subsequent runs (2/3 * current_value + 1/3 * previous_value = new)
+            else:
+                bpm = (((num_beats / window) * 60)*2 + prev_val) / 3
+                prev_val = bpm
+
+            # Timestamp = center of window
+            center_time = (i * samples_per_window + samples_per_window / 2) /  frame_rate
+            # Log results
+            results.append((int(center_time), int(bpm)))
+        return results
+    
+    def breathing_peak_detect(self, signal_data, frame_rate, window):
+        """
+        Divide breath_filtered into 20 second windows and count peaks in each.
+        
+        breath_filtered: 1D filtered breathing signal
+        fs_breath:       sample rate of breath signal (default 15 same as FRAME_RATE)
+        window_sec:      window size in seconds (default 20)
+        
+        returns: list of (time_sec, breaths_per_min) tuples
+        """
+        # Calculate total number of samples per window
+        samples_per_window = int(window * frame_rate)  # Default 300 samples at 15 Hz
+        # Total number of windows in recorded sample 
+        # >>>Sometimes total samples is slightly less than TIME * FRAME_RATE, check exact dimmensions for creating windows<<<
+        num_windows = (len(signal_data)+1) // samples_per_window
+        results = []
+        prev_val = 0
+        # Peaks Detection Per Window
+        for i in range(num_windows):
+            # Extract Window
+            current_window = signal_data[i * samples_per_window : (i + 1) * samples_per_window]
+            # Peaks Detection
+            # distance: minimum distance between peaks 2 * fs_breath = 2 * frames/second = 2 seconds
+            # This corresponds to max 30 breaths/min
+            # prominence = 0.15 = minimum amplitude for peak detection = 0.15
+            peaks, _ = find_peaks(current_window, distance=int(2 * frame_rate), prominence = 0.15)
+            # Mumber of peaks
+            num_breaths = len(peaks)
+            # Inter-window smoothing (weighted avg previous value with current value for new window)
+            # First run (no smoothing)
+            if prev_val == 0:
+                breaths_per_min = (num_breaths / window) * 60
+                prev_val = breaths_per_min
+            # Subsequent runs (2/3 * current_value + 1/3 * previous_value = new)
+            else:
+                breaths_per_min = (((num_breaths / window) * 60)*2 + prev_val) / 3
+                prev_val = breaths_per_min
+
+            # Timestamp = center of window
+            center_time = (i * samples_per_window + samples_per_window / 2) / frame_rate
+            # Log results
+            results.append((int(center_time), int(breaths_per_min)))
+        return results
+
+    #NEED ATTENTION: Currently bpm and breath return list of (time_sec, bpm) tuples. This requires a window to be defined to partition the signal into chunks.
+    # Each chunk is processed with the final bpm coming from a weighted sum of the current chunk and previous chunk bpm for smoothing.
+    # This should be changed so the previous value is passed into the original process-signal-pipeline function (starting at 0), and the bpm and breath values are simply a bpm output.
+    # Need to determine where this is called.
 
     def process_signal_pipeline(self, raw_signal):
         # Complete processing pipeline
@@ -171,28 +206,23 @@ class SignalProcessor:
         mti_filtered_heart = self.moving_target_indicator_heart(raw_signal)
         mti_filtered_breath = self.moving_target_indicator_breath(raw_signal)
 
-        # Step 2: Sliding average filter
-        filtered_heart = mti_filtered_heart
-        filtered_breath = mti_filtered_breath
-        # for _ in range(4):
-        filtered_heart = self.sliding_average_filter_heart(
-            filtered_heart, window_size=5
-        )
-        filtered_breath = self.sliding_average_filter_breath(filtered_breath, window=3)
+        # Step 2: Bandpass filtering
+        bp_filtered_heart = self.bandpass_filter_heart(mti_filtered_heart)
+        bp_filtered_breath = self.bandpass_filter_breath(mti_filtered_breath)
 
-        # Step 3: Bandpass filtering
-        bp_filtered_heart = self.bandpass_filter_heart(filtered_heart)
-        bp_filtered_breath = self.bandpass_filter_breath(filtered_breath)
+        # Step 3: Sliding average filter
+        filtered_heart = self.sliding_average_filter_heart(bp_filtered_heart, window_size=5)
+        filtered_breath = self.sliding_average_filter_breath(bp_filtered_breath, window=3)
 
-        # Step 4: Estimate heart rate
-        bpm = self.estimate_heart_rate_fft(bp_filtered_heart)
-        breath = self.estimate_breathing_rate_fft(bp_filtered_breath)
+        # Step 4: Estimate heart rate -ATTENTION: BASED ON 20 SAMPLE PARTITION WITHIN SIGNAL_DATA, SEE ABOVE COMMENT
+        bpm = self.heart_peak_detect(filtered_heart, self.sample_rate, window=20)
+        breath = self.breathing_peak_detect(filtered_breath, self.sample_rate, window=20)
 
         return {
-            "mti_filtered_heart": mti_filtered_heart,
-            "smoothed_heart": filtered_heart,
-            "bp_filtered_heart": bp_filtered_heart,
-            "bp_filtered_breath": bp_filtered_breath,
+            #"mti_filtered_heart": mti_filtered_heart,
+            #"bp_filtered_heart": bp_filtered_heart,
+            "filtered_heart": filtered_heart,
+            "filtered_breath": filtered_breath,
             "heart_rate_bpm": bpm,
             "breathing_rate": breath,
         }
